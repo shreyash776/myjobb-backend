826861aea002411fd7d14d18b6e160909a98532b
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-14 01:05:27 +0530

    refactor: reorganize imports and apply error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import { errorHandler } from './middlewares/error.middleware';
+import userRoutes from './routes/user.routes';
 dotenv.config();
 
 const app = express();
+app.use(errorHandler);
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
