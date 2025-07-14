537d1137253b9e63bd9efc3ef7c5720d10d2e697
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-15 00:08:31 +0530

    add the login route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { signup, verifyOtp, resendOtp,logout } from '../controllers/user.controller';
+import { signup, verifyOtp, resendOtp,logout, login } from '../controllers/user.controller';
 
 const router = Router();
-
+router.post("/login", login);
 router.post('/signup', signup);
 router.post('/verify-otp', verifyOtp);
 router.post('/resend-otp', resendOtp);
