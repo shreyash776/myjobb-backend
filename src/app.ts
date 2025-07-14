2273990062c071a2b77d4f973c7221bf1b37bb66
Shreyash chaurasia <shreyashchaurasia77@gmail.com>
2025-07-14 14:11:25 +0530

    feat: add demo route to serve welcome message for the API

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,11 @@ app.use(errorHandler);
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+
 app.use('/images', express.static(path.join(__dirname, '../images')));
+//demo route
+app.get('/', (req, res) => {
+    res.send('Welcome to MyJob Backend API');
+    });
 
 export default app;
